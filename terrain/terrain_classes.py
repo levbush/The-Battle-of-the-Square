66d1889350b6cda9aa35e1fc99717d37acff72c9
levbush <lev.o.bushuev@gmail.com>
2025-12-19 21:56:34 +0300

    terrain upd

--- terrain/terrain_classes.py
+++ terrain/terrain_classes.py
@@ -1,7 +1,17 @@
+from classes import City
+from unitclasses import UnitBase
+
+
 class TileBase:
     weight: int
-    def __init__(self):
-        self.terrain_type = self.__class__.__name__.lower()
+    def __init__(self, visible_mapping):
+        self.terrain_type = list(TERRAIN_TYPES.values()).index(self.__class__)
+        self.visible_mapping: list[bool] = visible_mapping
+        self.unit: UnitBase = None
+        self.city: City = None
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.visible_mapping})'
     
 
 class Land(TileBase):
@@ -26,7 +36,7 @@ TERRAIN_TYPES: dict[int, TileBase] = {0: Land, 1: Water, 2: Mountain}
 
 
 class Tile:
-    def __new__(cls, terrain_type):
+    def __new__(cls, terrain_type: int, player_count: int) -> TileBase:
         if terrain_type in TERRAIN_TYPES:
-            return TERRAIN_TYPES[terrain_type]()
+            return TERRAIN_TYPES[terrain_type]([False] * player_count)
         raise ValueError("Invalid terrain type")
