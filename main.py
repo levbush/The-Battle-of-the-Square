36db294af44fcbd52cb5c74f9f2e99fefc7c2834
levbush <lev.o.bushuev@gmail.com>
2025-12-19 21:58:29 +0300

    upd

--- main.py
+++ main.py
@@ -1,37 +1,21 @@
 import arcade
-import arcade.gui
-import tkinter as tk
+from screens.start_screen import StartView
+from database import init_dbs
 
 
-root = tk.Tk()
-SCREEN_WIDTH = root.winfo_screenwidth()
-SCREEN_HEIGHT = root.winfo_screenheight()
-
-SCREEN_TITLE = "Our game"
-
-class MainWindow(arcade.Window):
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title, fullscreen=True)
-        self.back_img = arcade.load_texture("images/back_images_in_menu.jpg")
-
-    def setup(self):
-        pass
-
-    def on_draw(self):
-        self.clear()
-        arcade.draw_texture_rect(self.back_img, arcade.rect.XYWH(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT), alpha=150)
-
-    def on_update(self, delta_time):
-        pass
+SCREEN_WIDTH, SCREEN_HEIGHT = arcade.window_commands.get_display_size()
+SCREEN_TITLE = "Battle of the Square"
 
 
 def setup_game(width=800, height=600, title="Battle of the Square"):
-    game = MainWindow(width, height, title)
-    game.setup()
-    return game
+    window = arcade.Window(width, height, title)
+    start_view = StartView()
+    window.show_view(start_view)
+    return window
 
 
 def main():
+    init_dbs()
     setup_game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
 
