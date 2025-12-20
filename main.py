9a592d3a865e44ed3ac1e5967666941aafe3a710
leopro <zhirnowl@gmail.com>
2025-12-20 23:55:19 +0300

    upd main

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ SCREEN_WIDTH, SCREEN_HEIGHT = arcade.window_commands.get_display_size()
 SCREEN_TITLE = "Battle of the Square"
 
 
+class MainWindow(arcade.Window):
+    # для событий, клавишь, мыши и тд.
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title, fullscreen=True)
+        self.is_fullscreen = True
+    
+    def on_key_press(self, key, modifiers):
+        if arcade.key.ESCAPE == key:
+            self.is_fullscreen = not(self.is_fullscreen)
+            self.set_fullscreen(self.is_fullscreen)
+            
+
 def setup_game(width=800, height=600, title="Battle of the Square"):
-    window = arcade.Window(width, height, title)
+    window = MainWindow(width, height, title)
     start_view = StartView()
     window.show_view(start_view)
     return window
